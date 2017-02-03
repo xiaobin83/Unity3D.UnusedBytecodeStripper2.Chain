ee4c07589363ec4422a1b893a10b1b1c7d792bb8
xiaobin83 <xiaobin.huang@gmail.com>
2017-02-03 10:34:36 +0800

    targeting sdk 3.5

--- UnusedBytecodeStripper2/Program.cs
+++ UnusedBytecodeStripper2/Program.cs
@@ -103,7 +103,7 @@ namespace UnusedBytecodeStripper2.Chain
 					var currentModulePath = Assembly.GetExecutingAssembly().Location;
 					var orgModulePath = currentModulePath.Substring(0, currentModulePath.Length - 3) + "org.exe";
 
-					var orgArgs = string.Join(" ", args.Select(a => '"' + a + '"'));
+					var orgArgs = string.Join(" ", args.Select(a => '"' + a + '"').ToArray());
 					Log(string.Format("Spawn: Exec={0}", orgModulePath));
 					Log(string.Format("       Args={0}", orgArgs));
 					var handle = Process.Start(orgModulePath, orgArgs);
@@ -120,7 +120,7 @@ namespace UnusedBytecodeStripper2.Chain
 					var currentModulePath = Assembly.GetExecutingAssembly().Location;
 					var orgModulePath = currentModulePath.Substring(0, currentModulePath.Length - 3) + "org.exe";
 
-					var orgArgs = '"' + orgModulePath + '"' + ' ' + string.Join(" ", args.Select(a => '"' + a + '"'));
+					var orgArgs = '"' + orgModulePath + '"' + ' ' + string.Join(" ", args.Select(a => '"' + a + '"').ToArray());
 					Log(string.Format("Spawn: Mono={0}", monoPath));
 					Log(string.Format("       Exec={0}", orgModulePath));
 					Log(string.Format("       Args={0}", orgArgs));
